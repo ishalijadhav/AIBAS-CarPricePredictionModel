a35f6e5a659262949dfca9299c3bf8289650e224
Ishali Jadhav <ishalijadhav20@gmail.com>
2025-02-04 00:55:57 +0100

    Building Docker Images

---
+++ images/codeBase_CarPricePrediction/RunAIModel.py
@@ -0,0 +1,24 @@
+import numpy
+import pandas as pd
+import tensorflow as tf
+import keras
+tf.config.set_visible_devices([], 'GPU')  # Disable GPU
+
+# activation_data = pd.read_csv("/tmp/activationBase/activation_data.csv", index_col=0)
+activation_data = pd.read_csv("/tmp/activationBase/activation_data.csv.csv")
+print(activation_data.columns)
+print("Activation data shape:", activation_data.shape)
+activation_data = activation_data.drop(columns=["selling_price"], axis=1)
+print("Activation data shape:", activation_data.shape)
+
+# model = tf.keras.models.load_model("/tmp/knowledgeBase/currentAiSolution.keras", compile=False)
+model = tf.keras.models.load_model("/tmp/knowledgeBase/currentAiSolution.keras", compile=False)
+print("Model input shape:", model.input_shape)
+
+activation_data = activation_data.to_numpy().reshape((-1, 16))
+
+predictions = model.predict(activation_data)
+
+denormalized_price = predictions * (10000000 - 29999) + 29999
+
+print("Selling Price:", round(denormalized_price[0][0],2))
