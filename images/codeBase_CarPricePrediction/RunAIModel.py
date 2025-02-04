36272d88f271530bc8f0c6fe45c98327dcba9eb8
Ishali Jadhav <ishalijadhav20@gmail.com>
2025-02-04 17:02:24 +0100

    Created docker-compose for AI Model

--- images/codeBase_CarPricePrediction/RunAIModel.py
+++ images/codeBase_CarPricePrediction/RunAIModel.py
@@ -4,16 +4,16 @@ import tensorflow as tf
 import keras
 tf.config.set_visible_devices([], 'GPU')  # Disable GPU
 
-# activation_data = pd.read_csv("/tmp/activationBase/activation_data.csv", index_col=0)
+# activation_data = pd.read_csv("data/activation_data.csv")
 activation_data = pd.read_csv("/tmp/activationBase/activation_data.csv")
-print(activation_data.columns)
-print("Activation data shape:", activation_data.shape)
+# print("Activation data cols:", activation_data.columns)
+# print("Activation data shape:", activation_data.shape)
 activation_data = activation_data.drop(columns=["selling_price"], axis=1)
-print("Activation data shape:", activation_data.shape)
+# print("Activation data shape:", activation_data.shape)
 
-# model = tf.keras.models.load_model("/tmp/knowledgeBase/currentAiSolution.keras", compile=False)
+# model = tf.keras.models.load_model("code/models/currentAiSolution.keras", compile=False)
 model = tf.keras.models.load_model("/tmp/knowledgeBase/currentAiSolution.keras", compile=False)
-print("Model input shape:", model.input_shape)
+# print("Model input shape:", model.input_shape)
 
 activation_data = activation_data.to_numpy().reshape((-1, 16))
 
